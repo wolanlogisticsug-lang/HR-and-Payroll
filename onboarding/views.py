596ce9c732cb9e77e393857c3088a13f2e521cd8
wolanlogisticsug-lang <wolanlogistics.ug@gmail.com>
2026-06-13 17:19:37 +0300

    commit

--- onboarding/views.py
+++ onboarding/views.py
@@ -2,7 +2,7 @@ import json
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from .models import InviteToken
 from .forms import HRInviteForm, CandidateOnboardingForm
 from .utils import send_onboarding_email
